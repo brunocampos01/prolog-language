f136f39605346bfa0df5fdac95799feb43371293
Bruno Campos <brunocampos01@gmail.com>
2018-09-18 12:00:28 -0300

    Delete descendentes.pl

--- exercícios/trabalho 01 - PROLOG/descendentes.pl
+++
@@ -1,9 +0,0 @@
-filha(marta,charlotte).
-filha(charlotte,caroline).
-filha(caroline,laura).
-filha(laura,rose).
-
-descendente(X,Y) :- filha(X,Y).
-descendente(X,Y) :- filha(X,Z),descendente(Z,Y).
-
- 
