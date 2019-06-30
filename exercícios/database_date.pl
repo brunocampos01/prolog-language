3741b58a022730fe7a6a03ab28ae00eb9e69b32c
brunocampos01 <brunocampos01@gmail.com>
2019-06-30 20:22:52 -0300

    feat(2019-06-30):

---
+++ exercícios/database_date.pl
@@ -0,0 +1,7 @@
+%Bruno Aurélio Rôzza de Moura Campos 14104255.
+%Generate database with date
+
+data(2019-06-30).
+data(2019-06-30).
+data(2019-06-30).
+data(2019-06-30).
---
+++ exercícios/prolog_database-2019-06-30.pl
@@ -0,0 +1,3 @@
+% Bruno Aurélio Rôzza de Moura Campos 14104255.
+ 
+key(idNd0FauVtpcCk-OaHM5xJ4pJpzVyzjwGjrHUzQPMyC19Mv9oj)
